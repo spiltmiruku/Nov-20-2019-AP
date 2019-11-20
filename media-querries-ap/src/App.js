c635ae5741a0e962dc759a07ab25da61e17362c2
spiltmiruku <kitsunedg@gmail.com>
2019-11-20 15:34:37 -0700

    Added basic content, responsive design wip

--- media-querries-ap/src/App.js
+++ media-querries-ap/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+library.add(faBars);
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+render(){
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<img src="https://manabox-global.com/wp-content/uploads/2016/08/vadim-sherbakov-30.jpg" alt="desk flat lay" id='background' />
+
+      <header id='header-container'>
+        <div className="logo"> Start Bootstrap </div>
+        <nav className="stickynavbar">
+          <nav id='nav-links'>
+          <a href='#services'>SERVICES</a>
+          <a href='#portfolio'>PORTFOLIO</a>
+          <a href='#about'>ABOUT</a>
+          <a href='team'>TEAM</a>
+          <a href='contact'>CONTACT</a>
+          </nav>
+          </nav>
+<button id='hamburgerbtn'>
+<FontAwesomeIcon icon='bars' id='hamburger' />
+</button>
+
+   <div id='welcome'> Welcome to Our Studio! </div>
+    <div id='meet'> IT'S NICE TO MEET YOU </div>
+    <button id='tmm'> Tell Me More </button>
       </header>
-    </div>
+      </div>
+    
   );
+}
 }
 
 export default App;
